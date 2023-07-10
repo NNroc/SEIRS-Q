8d96edba0c3902740097dc61c6c2fa0c71be3443
NNroc <1581333251@qq.com>
2023-07-10 14:57:38 +0800

    update

--- model/SEIRQD.py
+++ model/SEIRQD.py
@@ -11,8 +11,9 @@ import xlwt
 class SEIRQD:
     def __init__(self, data: dict, a: dict, time: dict, real_patients: dict,
                  r_is=20.0, r_ia=40.0, beta_is=0.05, beta_ia=0.025,
-                 t=1.0, alpha=4.4, i=3.0, c=0.4,
-                 theta_s=0.8, theta_a=0.6, gamma_s1=10.0, gamma_a1=10.0, gamma_u=10.0, p=0.15, m=0.064):
+                 t=1.0, alpha=4.4, c=0.4,
+                 theta_s=0.8, theta_a=0.6, gamma_s1=10.0, gamma_a1=10.0, gamma_u=10.0, p=0.15, m=0.064,
+                 al=0, q=0.2):
         """
         :param data: SEIR
         :param a: 人口净流动量 原始数据集
@@ -24,18 +25,18 @@ class SEIRQD:
         :param beta_ia: 无症状感染系数 例：0.025 基于SEIR模型的高校新冠肺炎疫情传播风险管控研究
         :param t: 流动人口中的易感者比例	初设0（有隔离政策）、0.00001（无隔离政策）
         :param alpha: 潜伏期	根据毒株而定
-        :param i: 核酸检测频率(几天一次)，初设1-7天、0天（即不检测，自然发生对照组）
         :param c: 有症状比例 0.4
         :param theta_s: 有症状核酸检出率 0.8
         :param theta_a: 无症状核酸检出率 0.6
         :param gamma_s1: 中轻度患者痊愈时间 10
-        :param gamma_a1: 中轻度无症状患者痊愈时间 10
+        :param gamma_a1: 无症状患者痊愈时间 10
         :param gamma_u: 重症患者接受治疗后的痊愈时间 10
+        :param al: 抗体水平随时间变化的表达式
+        :param q: 自我隔离比例
         :param p: 中轻度患者重症率 15%
         :param m: 重症患者死亡率 6.4%
         """
         self.data = data
-        self.a = a
         self.time = time
         self.real_patients = real_patients
         self.r_is = r_is
@@ -44,9 +45,7 @@ class SEIRQD:
         self.beta_ia = beta_ia
         self.r_beta_is = self.r_is * self.beta_is
         self.r_beta_ia = self.r_ia * self.beta_ia
-        self.t = t
         self.alpha = alpha
-        self.i = i
         self.c = c
         self.theta_s = theta_s
         self.theta_a = theta_a
@@ -55,132 +54,57 @@ class SEIRQD:
         self.gamma_u = gamma_u
         self.p = p
         self.m = m
-
-        if self.i == 0:
-            self.theta_s = 0.0
-            self.theta_a = 0.0
+        self.al = al
+        self.q = q
 
     def run(self):
         for indx in range(len(self.time) - 1):
             # 易感者
             susceptible = - (self.r_beta_is * self.data["susceptible"][indx] * self.data["infectious_s"][indx]
                              + self.r_beta_ia * self.data["susceptible"][indx] * self.data["infectious_a"][indx]) / \
-                          self.data["n"] + self.t * self.a[indx] \
-                          + self.data["susceptible"][indx]
-            if susceptible <= 0.0:
-                susceptible = 0.0
-
+                          self.data["n"] + self.al * self.data["recovered"][indx] + self.data["susceptible"][indx]
             # 暴露者
             exposed = (self.r_beta_is * self.data["susceptible"][indx] * self.data["infectious_s"][indx]
                        + self.r_beta_ia * self.data["susceptible"][indx] * self.data["infectious_a"][indx]) / \
-                      self.data["n"] - self.data["exposed"][indx] / self.alpha + (1.0 - self.t) * self.a[indx] \
-                      + self.data["exposed"][indx]
-            if exposed <= 0.0:
-                exposed = 0.0
-
+                      self.data["n"] - self.data["exposed"][indx] / self.alpha + self.data["exposed"][indx]
             # 感染者 中轻度患者
-            # i_0 核酸检测为0及不为0时的情况
-            if self.i == 0:
-                infectious_s = self.c * self.data["exposed"][indx] / self.alpha \
-                               - self.data["infectious_s"][indx] / self.gamma_s1 \
-                               - self.p * self.data["infectious_s"][indx] \
-                               + self.data["infectious_s"][indx]
-            else:
-                infectious_s = self.c * self.data["exposed"][indx] / self.alpha \
-                               - self.theta_s * self.data["infectious_s"][indx] / self.i \
-                               + self.data["infectious_s"][indx]
-            if infectious_s <= 0.0:
-                infectious_s = 0.0
-
+            infectious_s = self.c * self.data["exposed"][indx] / self.alpha \
+                           - self.data["infectious_s"][indx] / self.gamma_s1 \
+                           - self.data["infectious_s"][indx] * self.q \
+                           + self.data["infectious_s"][indx]
             # 感染者 无症状患者
-            # i_0 核酸检测为0及不为0时的情况
-            if self.i == 0:
-                infectious_a = (1.0 - self.c) * self.data["exposed"][indx] / self.alpha \
-                               - self.data["infectious_a"][indx] / self.gamma_a1 \
-                               + self.data["infectious_a"][indx]
-            else:
-                infectious_a = (1.0 - self.c) * self.data["exposed"][indx] / self.alpha \
-                               - self.theta_a * self.data["infectious_a"][indx] / self.i \
-                               + self.data["infectious_a"][indx]
-            if infectious_a <= 0.0:
-                infectious_a = 0.0
-
-            # 感染者 重症状患者
-            # i_0 核酸检测为0及不为0时的情况
-            if self.i == 0:
-                infectious_u = self.p * self.data["infectious_s"][indx] \
-                               - self.data["infectious_u"][indx] / self.gamma_u \
-                               - self.m * self.data["infectious_u"][indx] \
-                               + self.data["infectious_u"][indx]
-            else:
-                infectious_u = self.p * self.data["quarantine_s"][indx] \
-                               - self.data["infectious_u"][indx] / self.gamma_u \
-                               - self.m * self.data["infectious_u"][indx] \
-                               + self.data["infectious_u"][indx]
-            if infectious_u <= 0.0:
-                infectious_u = 0.0
-
-            # 感染者 中轻度隔离患者
-            if self.i == 0:
-                quarantine_s = 0.0
-            else:
-                quarantine_s = self.theta_s * self.data["infectious_s"][indx] / self.i \
-                               - self.p * self.data["quarantine_s"][indx] - self.data["quarantine_s"][
-                                   indx] / self.gamma_s1 \
-                               + self.data["quarantine_s"][indx]
-            if quarantine_s <= 0.0:
-                quarantine_s = 0.0
-
-            # 感染者 无症状隔离患者
-            if self.i == 0:
-                quarantine_a = 0.0
-            else:
-                quarantine_a = self.theta_a * self.data["infectious_a"][indx] / self.i \
-                               - self.data["quarantine_a"][indx] / self.gamma_a1 \
-                               + self.data["quarantine_a"][indx]
-            if quarantine_a <= 0.0:
-                quarantine_a = 0.0
-
+            infectious_a = (1.0 - self.c) * self.data["exposed"][indx] / self.alpha \
+                           - self.data["infectious_a"][indx] / self.gamma_a1 \
+                           + self.data["infectious_a"][indx]
+            # 自我隔离者
+            quarantine = self.q * self.data["infectious_s"][indx] - self.data["quarantine"][indx] / self.gamma_s1 \
+                         + self.data["quarantine"][indx]
             # 康复者
-            if self.i == 0:
-                recovered = self.data["infectious_u"][indx] / self.gamma_u \
-                            + self.data["infectious_s"][indx] / self.gamma_s1 \
-                            + self.data["infectious_a"][indx] / self.gamma_a1 \
-                            + self.data["recovered"][indx]
-            else:
-                recovered = self.data["infectious_u"][indx] / self.gamma_u \
-                            + self.data["quarantine_s"][indx] / self.gamma_s1 \
-                            + self.data["quarantine_a"][indx] / self.gamma_a1 \
-                            + self.data["recovered"][indx]
-            if recovered <= 0.0:
-                recovered = 0.0
-
-            # 死亡者
-            dead = self.m * self.data["infectious_u"][indx] \
-                   + self.data["dead"][indx]
-            if dead <= 0.0:
-                dead = 0.0
+            recovered = self.data["infectious_s"][indx] / self.gamma_s1 \
+                        + self.data["infectious_a"][indx] / self.gamma_a1 \
+                        + self.data["quarantine"][indx] / self.gamma_s1 \
+                        - self.al * self.data["recovered"][indx] \
+                        + self.data["recovered"][indx]
 
             self.data["susceptible"].append(float(susceptible))
             self.data["exposed"].append(float(exposed))
             self.data["infectious_s"].append(float(infectious_s))
             self.data["infectious_a"].append(float(infectious_a))
-            self.data["infectious_u"].append(float(infectious_u))
-            self.data["quarantine_s"].append(float(quarantine_s))
-            self.data["quarantine_a"].append(float(quarantine_a))
+            # self.data["infectious_u"].append(float(infectious_u))
+            self.data["quarantine"].append(float(quarantine))
             self.data["recovered"].append(float(recovered))
-            self.data["dead"].append(float(dead))
+            # self.data["dead"].append(float(dead))
 
-            self.data["predict_total"].append(self.data["infectious_u"][indx + 1]
-                                              + self.data["quarantine_s"][indx + 1]
-                                              + self.data["quarantine_a"][indx + 1])
-            self.data["predict_all"].append(self.data["infectious_s"][indx + 1]
-                                            + self.data["infectious_a"][indx + 1]
-                                            + self.data["infectious_u"][indx + 1]
-                                            + self.data["quarantine_s"][indx + 1]
-                                            + self.data["quarantine_a"][indx + 1]
-                                            + self.data["recovered"][indx + 1]
-                                            + self.data["dead"][indx + 1])
+            # self.data["predict_total"].append(self.data["infectious_u"][indx + 1]
+            #                                   + self.data["quarantine_s"][indx + 1]
+            #                                   + self.data["quarantine_a"][indx + 1])
+            # self.data["predict_all"].append(self.data["infectious_s"][indx + 1]
+            #                                 + self.data["infectious_a"][indx + 1]
+            #                                 + self.data["infectious_u"][indx + 1]
+            #                                 + self.data["quarantine_s"][indx + 1]
+            #                                 + self.data["quarantine_a"][indx + 1]
+            #                                 + self.data["recovered"][indx + 1]
+            #                                 + self.data["dead"][indx + 1])
 
     def train(self, beta_is=None, beta_ia=None):
         if beta_is is None or beta_ia is None:
