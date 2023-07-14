e5ebb7f02332d94054bfa05f5e9d3c73a5195c8c
NNroc <1581333251@qq.com>
2023-07-14 10:38:34 +0800

    change bug

--- main_predict.py
+++ main_predict.py
@@ -6,7 +6,7 @@ import argparse
 from model.SEIRQD import SEIRQD
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--city_name', type=str, default='beijing',
+parser.add_argument('--city_name', type=str, default='world',
                     help='To predict the urban epidemic information by name')
 # month2：两个月
 # year：一年
@@ -17,7 +17,7 @@ args = parser.parse_args()
 # 新十条2022年12月7日数据
 seir_data_china = {
     "n": 1400000000,  # 该城市人口总数
-    "city_name": "北京",  # 城市名称
+    "city_name": "world",  # 城市名称
     "susceptible": [1400000000.0],  # 易感者
     "exposed": [0.0],  # 暴露者
     "infectious_s": [4079.0],  # 感染者 中轻度患者
@@ -33,11 +33,10 @@ seir_data_china = {
 }
 
 # 获取数据，开始运行
-folder = args.folder
 ans = SEIRQD(copy.deepcopy(seir_data_china), time=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
              r_is=60.0, r_ia=40.0, beta_is=0.5, beta_ia=0.5, alpha=3.0, c=0.7, q=0.03, al=0.0083,
              gamma_s1=10.0, gamma_a1=7.0)
 ans.train()
 ans.data["predict_total"] = [int(i) for i in ans.data["predict_total"]]
-ans.drawGraph()
+# ans.drawGraph()
 ans.saveResultToExcel()
